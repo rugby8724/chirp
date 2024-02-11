5b3f99f3be1471bf5f0ffd6899d86ca007d48eb3
rugby8724 <jeremiah.d.wise@gmail.com>
2024-02-11 16:14:25 -0600

    Add auth

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,11 +2,13 @@ import { Client } from "@planetscale/database";
 import { PrismaPlanetScale } from "@prisma/adapter-planetscale";
 import { PrismaClient } from "@prisma/client";
 
+
 import { env } from "~/env.js";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
+
 
 const client = new Client({ url: env.DATABASE_URL });
 
@@ -15,7 +17,6 @@ export const db =
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-    adapter: new PrismaPlanetScale(client),
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
